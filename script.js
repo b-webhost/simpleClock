08f6362089290643facf0113fe55f9a295ec6123
DionysusBenstein <dionysusbenstein@gmail.com>
2020-08-14 09:32:52 +0300

    add icon

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function setTime() {
     let element = document.getElementById("time");
     let now = new Date();
+
     element.innerHTML = now.toLocaleTimeString();
+    /*document.body.style.background = "#aaaaaa";*/
     setInterval(setTime, 1000);
 }
 
